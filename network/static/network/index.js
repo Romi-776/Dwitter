ae10bdc1ca71f0ca1febc01d946b41563194a301
Romi-776 <lakhbir.ls3@gmail.com>
2021-05-19 17:44:12 +0530

    User now have option to change his name, bio, profile and background pic but not really

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Modal related even listener which will work only if user
+    // Modal related event listener which will work only if user
     // wants to edit his post
-    var exampleModal = document.getElementById('exampleModal')
-    exampleModal.addEventListener('show.bs.modal', function (event) {
-
+    var postModal = document.getElementById('postModal')
+    postModal.addEventListener('show.bs.modal', function (event) {
+    
         // Button that triggered the modal
         var button = event.relatedTarget
-
+    
         // Extract info from data-bs-* attributes
         var recipient = button.getAttribute('data-bs-post_owner')
         var content = button.getAttribute('data-bs-post')
         var post_no = button.getAttribute('data-bs-post_no')
-
+    
         // Update the modal's content.
-        var modalTitle = exampleModal.querySelector('.modal-title')
-        var modalBodyTextarea = exampleModal.querySelector('.modal-body textarea')
-
+        var modalTitle = postModal.querySelector('.modal-title')
+        var modalBodyTextarea = postModal.querySelector('.modal-body textarea')
+    
         modalTitle.textContent = 'Posted by - ' + recipient
         modalBodyTextarea.value = content
-
+    
         // save the changes that i had made in the textarea
-        var saveChangesButton = exampleModal.querySelector('#save-changes')
-
+        var saveChangesButton = postModal.querySelector('#save-changes')
+    
         /*
             This will change the description of the post when user
             hits the save changes button
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // changing the post's description from html
             var data = modalBodyTextarea.value
             document.querySelector(`#post-${post_no}`).innerHTML = data
-
+    
             // updating it in the db
             fetch('/update_post', {
                 method: 'POST',
@@ -43,10 +43,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 .then(response => response.json())
                 .then(result => {
                 })
-
+    
             document.querySelector('#close-modal').click()
             return true;
         }
+    })
+
+
+    var profileModal = document.getElementById('profileModal')
+    profileModal.addEventListener('show.bs.modal', function (event) {
+    
+        // Button that triggered the modal
+        var button = event.relatedTarget
+    
+        // Extract info from data-bs-* attributes
+
+        
     })
 })
 
