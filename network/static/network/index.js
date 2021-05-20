46be4075b83f2df3d3a015a34eceb0d9e7497ccb
Romi-776 <lakhbir.ls3@gmail.com>
2021-05-20 12:17:08 +0530

    New UI to upload the profile and background image but can't update right now

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,28 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    document.getElementById("change-background").addEventListener('click', () => {
+        alert('button is clicked')
+        document.getElementById('background_img').click()
+        return false
+    })
+
     // Modal related event listener which will work only if user
     // wants to edit his post
     var postModal = document.getElementById('postModal')
     postModal.addEventListener('show.bs.modal', function (event) {
-    
+
         // Button that triggered the modal
         var button = event.relatedTarget
-    
+
         // Extract info from data-bs-* attributes
         var recipient = button.getAttribute('data-bs-post_owner')
         var content = button.getAttribute('data-bs-post')
         var post_no = button.getAttribute('data-bs-post_no')
-    
+
         // Update the modal's content.
         var modalTitle = postModal.querySelector('.modal-title')
         var modalBodyTextarea = postModal.querySelector('.modal-body textarea')
-    
+
         modalTitle.textContent = 'Posted by - ' + recipient
         modalBodyTextarea.value = content
-    
+
         // save the changes that i had made in the textarea
         var saveChangesButton = postModal.querySelector('#save-changes')
-    
+
         /*
             This will change the description of the post when user
             hits the save changes button
@@ -31,19 +37,19 @@ document.addEventListener('DOMContentLoaded', () => {
             // changing the post's description from html
             var data = modalBodyTextarea.value
             document.querySelector(`#post-${post_no}`).innerHTML = data
-    
+
             // updating it in the db
             fetch('/update_post', {
                 method: 'POST',
                 body: JSON.stringify({
-                    post_id : `${post_no}`,
+                    post_id: `${post_no}`,
                     updated_description: `${data}`
                 })
             })
                 .then(response => response.json())
                 .then(result => {
                 })
-    
+
             document.querySelector('#close-modal').click()
             return true;
         }
@@ -52,13 +58,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
     var profileModal = document.getElementById('profileModal')
     profileModal.addEventListener('show.bs.modal', function (event) {
-    
+
         // Button that triggered the modal
         var button = event.relatedTarget
-    
+
         // Extract info from data-bs-* attributes
 
-        
+
+    })
+
+    var profilePicModal = document.getElementById('profilePicModal')
+    profilePicModal.addEventListener('show.bs.modal', function (event) {
+
+        // Button that triggered the modal
+        var button = event.relatedTarget
+
+        // Extract info from data-bs-* attributes
+
+
     })
 })
 
