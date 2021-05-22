b6a5db29274aa675b4f859ee4ea6ec8ff3e0efc3
Romi-776 <lakhbir.ls3@gmail.com>
2021-05-22 06:42:02 +0530

    Added a field to store background image of the user

--- network/models.py
+++ network/models.py
@@ -5,14 +5,22 @@ from django.db import models
 class User(AbstractUser):
     """Model class for USER related details"""
 
+    # it will contain basic user details like
+    # username, password, email, etc.
     pass
 
 
 class Profile(models.Model):
+    # user who's profile is this
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name="user_profile"
     )
+
     profile_pic = models.ImageField(default="default.jpg", upload_to="profile_pics")
+
+    background_img = models.ImageField(
+        default="Default_background.jpg", upload_to="bg_img"
+    )
 
     def __str__(self):
         return f"{self.user.username} Profile"
@@ -24,7 +32,9 @@ class post(models.Model):
     posted_by = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="post_owner"
     )
+
     posted_on = models.DateTimeField(auto_now_add=True)
+
     description = models.TextField(null=False, default="")
 
     def __str__(self):
@@ -37,6 +47,7 @@ class follower_following(models.Model):
     followers = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="my_followers"
     )
+
     following = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="my_following"
     )
@@ -48,10 +59,14 @@ class follower_following(models.Model):
 class likes(models.Model):
     """Model class for likes related details"""
 
+    # post which is liked
     on_which_post = models.ForeignKey(
         post, on_delete=models.CASCADE, related_name="likes_on_this_post"
     )
+
     when = models.DateTimeField(auto_now_add=True)
+
+    # user who liked
     who = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="who_liked_this_post"
     )
